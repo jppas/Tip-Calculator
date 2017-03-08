fe4606d8e92fad0d69f533d6385dc6b441856e44
Peevee Jace G. Pascual <peevee12@yahoo.com>
2017-03-08 16:50:22 -0700

    Updated IOS Project

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -16,7 +16,7 @@ namespace TipCalculator.iOS
 			set;
 		}
 
-		pude bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+		public bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
 		{
 			// Override point for customization after application launch.
 			// If not required for your application you can safely delete this method
